d32740d2491130a4924083c3213d4e272dc89498
Moaid Hashem <Gamestorrent2015@gmail.com>
2024-08-01 14:39:10 +0300

    Adding new branch & creating students array first element

---
+++ script.js
@@ -0,0 +1,10 @@
+let students = [
+    {
+        name: "moaid",
+        age: 22,
+        email:"moaid@gmail.com",
+        track:"MERN"
+    },
+   
+];
+localStorage.setItem('member1', JSON.stringify(students[0]));
